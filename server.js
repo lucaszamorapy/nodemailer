198881400977f3099de2611c673a23b7ffce2ce8
lucaszamorapy <lucaszamora.zulmira@gmail.com>
2024-08-10 11:02:00 -0300

    chore: initialization

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const cors = require("cors");
+const routes = require("./routes/routes");
+const sequelize = require("./config/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", routes);
+
+app.listen(8082, () => {
+  console.log("Server is running on port 8082");
+});
+
+sequelize.sync({ force: false }).then(() => {
+  console.log("Database and tables created");
+});
